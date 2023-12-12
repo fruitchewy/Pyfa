e2f836c2170dff24156b2f6d659dd45f686fe65f
Austin Kauble <32265025+fruitchewy@users.noreply.github.com>
2023-12-12 11:59:27 -0800

    Update osx-package.sh

--- scripts/osx-package.sh
+++ scripts/osx-package.sh
@@ -12,7 +12,7 @@ echo "${PYFA_VERSION}"
 echo "Pyfa version (YAML):"
 cat version.yml
 echo "Building distributive..."
-python3 -m PyInstaller --target-arch $arch -y --clean dist_assets/mac/pyfa.spec
+python3 -m PyInstaller -y --clean dist_assets/mac/pyfa.spec
 echo "Compressing distributive..."
 cd dist
 zip -r "pyfa-$PYFA_VERSION-mac.zip" pyfa.app
