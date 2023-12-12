07a80ffc3e935e5bd28f26467a6ab49ca9d70c8a
Austin Kauble <32265025+fruitchewy@users.noreply.github.com>
2023-12-12 11:59:08 -0800

    Update osx-setup.sh

--- scripts/osx-setup.sh
+++ scripts/osx-setup.sh
@@ -12,6 +12,6 @@ python3 --version
 echo "Upgrading pip..."
 python3 -m pip install --upgrade pip
 echo "Installing app requirements..."
-python3 -m pip install --platform=$arch --only-binary=:all: --target build/py/ -r requirements.txt 
+python3 -m pip install -r requirements.txt 
 echo "Installing packaging tools..."
 python3 -m pip install PyInstaller==6.2.0 
